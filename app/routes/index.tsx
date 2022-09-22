389dad75a0ff3e400be55d78f9fdd512fb54df41
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2022-09-22 08:07:28 +0530

    feat: added initial home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <p>This is the index page</p>
+    <main className="relative h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <h1 className="font-bold text-xl">Hi! there, I'm Muthukumar.</h1>
     </main>
   );
 }
